349134d2787db3f5ae895845d9daf3c611fe1cfc
Ulveboss <mikkel@iploug.dk>
2025-04-24 09:53:49 +0200

    minor

--- script.js
+++ script.js
@@ -7,37 +7,23 @@ const nextButton = document.getElementById('nextButton');
 
 
 fetch('scenarios.json')
+    .then(response => response.json())
+    .then(data => {
+        allScenarios = data;
+        console.log(allScenarios)
+        start()
+    })
+    .catch(error => {
+        console.error('Fejl ved hentning af JSON:', error);
+    });
 
-  .then(response => response.json())
 
-  .then(data => {
-
-
-    allScenarios = data;
-
-    console.log(allScenarios)
-
-    start()
-
-    // showQuestion()
-
-  })
-
-  .catch(error => {
-
-    console.error('Fejl ved hentning af JSON:', error);
-
-  });
-  
-
-  function showQuestion(id) {
+function showQuestion(id) {
     const currentQuestion = currentScenario[id]
-
-    if(!currentQuestion){
+    if (!currentQuestion) {
         questionElement.textContent = "Scenariet er slut lil bro"
         answersElement.innerHTML = ""
     }
-
     questionElement.textContent = currentQuestion.text
     answersElement.innerHTML = ""
 
@@ -50,26 +36,25 @@ fetch('scenarios.json')
             answersElement.appendChild(button)
         })
     }
+}
 
-  }
-
-  function start() {
+function start() {
     questionElement.textContent = "Vælg et scenarie:";
     answersElement.innerHTML = "";
-  
+
     const scenarioList = Object.entries(allScenarios);
-  
+
     scenarioList.forEach(([scenarioName, scenarioContent]) => {
-      const button = document.createElement('button');
-      button.textContent = scenarioName;
-      button.classList.add('answerButton');
-      button.onclick = () => {
-        currentScenario = scenarioContent;
-        showQuestion("start");
-      };
-      answersElement.appendChild(button);
+        const button = document.createElement('button');
+        button.textContent = scenarioName;
+        button.classList.add('answerButton');
+        button.onclick = () => {
+            currentScenario = scenarioContent;
+            showQuestion("start");
+        };
+        answersElement.appendChild(button);
     });
-  }
-    function returner() {
-        start()
-    }
+}
+function returner() {
+    start()
+}
